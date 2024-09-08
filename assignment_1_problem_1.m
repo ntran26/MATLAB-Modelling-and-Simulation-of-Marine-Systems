86fbc1a7fd18c62cb8b2f4df49948cda277949b9
ntran26 <hoangnamtran2610@gmail.com>
2024-09-08 23:26:42 +1000

    udpate

--- assignment_1_problem_1.m
+++ assignment_1_problem_1.m
@@ -1,52 +1,101 @@
+% Author: Nam Tran
+% This program plots the armature current, speed, shaft angle over time.
+% Since the input voltage is constant, we don't plot it.
+% This program uses 2 methods: Built-in ODE solver and Euler's method.
+
 clear, clc, close all
 
-% Parameters
-R = 0.25;       % Resistance (Ohms)
-L = 0.005;      % Inductance 
-Kt = 36.3e-3;   % Torque constant (Nm/A)
-Ke = 1/Kt;      % Back EMF constant (V/(rad/s))
-J = 1.68e-6;    % Rotor Inertia (kg.m^2)
-B = 1e-6;       % Damping coefficient (N.m.s/rad, estimated)
-Va = 48;        % Applied voltage (V)
+% Built-in solver
+tspan = [0 2];     % time span
 
-% Transfer function between shaft speed and input voltage
-num = [Kt];
-den = [J*L, (B*L + R*J), (B*R + Kt*Ke)];
-sys_speed = tf(num, den);
+u = [48; 0];       % input voltage step
 
-% Transfer function between shaft angle and input voltage
-sys_angle = tf(num, [den 0]);
+% Initial conditions [angle, speed, armature current]
+x0 = [0; 0; 0];
 
-% Simulation time
-t = 0:0.001:1;
+% Solve using ode45 (built-in solver)
+[t, x] = ode45(@problem1, tspan, x0, [], u);
 
-% Step response
+% Plot results
 figure;
-subplot(2,1,1);
-step(sys_speed, t);
-title('Step Response: Shaft Speed vs Input Voltage');
-ylabel('Speed (rad/s)');
-xlabel('Time (s)');
+subplot(3,1,1);
+plot(t, x(:,3), "LineWidth", 2); 
+% xlabel('Time (s)');
+ylabel('Current (A)'); 
+title('Armature Current');
 
-subplot(2,1,2);
-step(sys_angle, t);
-title('Step Response: Shaft Angle vs Input Voltage');
+subplot(3,1,2);
+plot(t, x(:,2), "LineWidth", 2);
+% xlabel('Time (s)');
+ylabel('Speed (rad/s)');
+title('Motor Speed');
+
+subplot(3,1,3);
+plot(t, x(:,1), "LineWidth", 2);
+xlabel('Time (s)');
 ylabel('Angle (rad)');
-xlabel('Time (s)');
+title('Shaft Angle');
 
-% State-space representation
-A = [-B/J, Kt/J; -Ke/L, -R/L];
-B = [0; 1/L];
-C = [1, 0];
-D = [0];
-sys_ss = ss(A, B, C, D);
 
-% Time response for a step input
-[y, t, x] = step(sys_ss, t);
 
+
+
+% Euler Method
+R = 3.72;       % Resistance (Ohms)
+L = 1.04e-3;    % Inductance (H)
+K = 0.068;      % Torque constant (Nm/A)
+J = 64.5e-7;    % Rotor Inertia (kg.m^2)
+b = 1.29e-3;    % Damping coefficient (kgm^2/s, estimated)
+Va = 48;        % Input voltage (V)
+
+% Start and final time and step
+ts = 0;
+tf = 2;
+h = 0.0001;
+n = tf/h;
+tspan = ts:h:tf;
+
+% Initial conditions [angle; speed; current]
+x = zeros(3, length(tspan));  % [x1 = angle, x2 = speed, x3 = current]
+x(:,1) = [0; 0; 0];
+
+% Euler method loop
+for k = 1:n
+    x(1,k+1) = x(1,k) + h * x(2,k);     % Update angle
+    x(2,k+1) = x(2,k) + h * (-b/J * x(2,k) + K/J * x(3,k));  % Update speed
+    x(3,k+1) = x(3,k) + h * (-K/L * x(2,k) - R/L * x(3,k) + Va/L);  % Update current
+end
+
+% Plot the results
 figure;
-plot(t, y);
-title('State-Space Response: Shaft Speed');
-xlabel('Time (s)');
+subplot(3,1,1);
+plot(tspan, x(3,:), "LineWidth", 2);
+% xlabel('Time (s)');
+ylabel('Current (A)');
+title('Armature Current');
+
+subplot(3,1,2);
+plot(tspan, x(2,:), "LineWidth", 2);
+% xlabel('Time (s)');
 ylabel('Speed (rad/s)');
-grid on;
+title('Motor Speed');
+
+subplot(3,1,3);
+plot(tspan, x(1,:), "LineWidth", 2);
+xlabel('Time (s)');
+ylabel('Angle (rad)');
+title('Shaft Angle');
+
+function xdot = problem1(t,x,u)
+    R = 3.72;       % Resistance (Ohms)
+    L = 1.04e-3;    % Inductance (H)
+    K = 0.068;      % Torque constant (Nm/A)
+    J = 64.5e-7;    % Rotor Inertia (kg.m^2)
+    b = 1.29e-3;    % Damping coefficient (kgm^2/s, estimated)
+
+    % State equations
+    xdot = [x(2);
+            -b/J*x(2) + K/J*x(3);
+            -K/L*x(2) - R/L*x(3) + 1/L*u(1)];
+end
+
